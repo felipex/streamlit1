c0f07ee0017ad246981770c537429c28068dabad
Felipe Cavalcante <felipexcavalcante@gmail.com>
2025-05-02 17:49:40 +0000

    Atualização da paágina inicial

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import requests
+import pandas as pd
+import json
 
 unidade = "122391"  #UFCA
 #estrutura = requests.get(
@@ -19,4 +21,75 @@ st.image(
 st.write(
     "Dados obtidos do [Portal da Transparência](http://www.portaltransparencia.gov.br"
 )
-st.write(estrutura)
+#st.write(estrutura)
+
+#################
+unidade = "122391"  #UFCA
+
+estrutura = requests.get(
+    f"http://estruturaorganizacional.dados.gov.br/doc/unidade-organizacional/{unidade}/estrutura"
+)
+estrutura = estrutura.json()
+
+
+def orgao(codigo):
+  if codigo == 0:
+    return {'codigo': 0, 'nome': 'Não informado', 'sigla': 'Não informado'}
+  estrutura = requests.get(
+      f"http://estruturaorganizacional.dados.gov.br/doc/unidade-organizacional/{codigo}"
+  )
+  estrutura = estrutura.json()
+  estrutura = estrutura['unidade']
+  return {
+      'SETOR_CODIGO': codigo,
+      'SETOR_NOME': estrutura['nome'],
+      'SIGLA': estrutura['sigla']
+  }
+
+
+df_setor = pd.DataFrame(columns=[
+    'SETOR_CODIGO', 'SETOR_NOME', 'CAMINHO', 'UNIDADE_SIGLA', 'UNIDADE_NOME'
+])
+
+
+#
+def pstring(codigo, nome, path, codigo_superior, nome_superior):
+  df_setor.loc[len(df_setor)] = [
+      codigo, nome, path, codigo_superior, nome_superior
+  ]
+  return f'''"{codigo}", "{nome}", "{path}", "{codigo_superior}", "{nome_superior}" '''
+
+
+for e in estrutura['estrutura']['estrutura']:
+  o = orgao(e['codigoUnidade'])
+  print(
+      pstring(o['SETOR_CODIGO'], o['SETOR_NOME'], f"{o['SIGLA']}/UFCA",
+              o['SIGLA'], o['SETOR_NOME']))
+
+  if e['estrutura']:
+    for ee in e['estrutura']:
+      oo = orgao(ee['codigoUnidade'])
+
+      print(
+          pstring(oo['SETOR_CODIGO'], oo['SETOR_NOME'],
+                  f"{oo['SIGLA']}/{o['SIGLA']}/UFCA", o['SIGLA'],
+                  o['SETOR_NOME']))
+
+      if ee['estrutura']:
+        for eee in ee['estrutura']:
+          ooo = orgao(eee['codigoUnidade'])
+          print(
+              pstring(ooo['SETOR_CODIGO'], ooo['SETOR_NOME'],
+                      f"{ooo['SIGLA']}/{oo['SIGLA']}/{o['SIGLA']}/UFCA",
+                      o['SIGLA'], o['SETOR_NOME']))
+
+          if eee['estrutura']:
+            for eeee in eee['estrutura']:
+              oooo = orgao(eeee['codigoUnidade'])
+              print(
+                  pstring(
+                      oooo['SETOR_CODIGO'], oooo['SETOR_NOME'],
+                      f"{oooo['SIGLA']}/{ooo['SIGLA']}/{oo['SIGLA']}/{o['SIGLA']}/UFCA",
+                      o['SIGLA'], o['SETOR_NOME']))
+
+st.write(df_setor)
