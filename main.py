4c24cfecbcd1f2704689ba3639a2120ce70e2569
Felipe Cavalcante <felipexcavalcante@gmail.com>
2025-05-02 14:26:56 +0000

    testes com endpoints

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import streamlit as st
 import requests
 
 unidade = "122391"  #UFCA
-
+#estrutura = requests.get(
+#    f"http://estruturaorganizacional.dados.gov.br/doc/unidade-#organizacional/{unidade}/estrutura"
+#)
 estrutura = requests.get(
-    f"http://estruturaorganizacional.dados.gov.br/doc/unidade-organizacional/{unidade}/estrutura"
+    #"https://estruturaorganizacional.dados.gov.br/doc/unidade-organizacional/122391/completa"
+    "https://estruturaorganizacional.dados.gov.br/doc/estrutura-organizacional/resumida?codigoPoder=1&codigoEsfera=1&codigoUnidade=122391"
 )
+
 estrutura = estrutura.json()
 
 st.title("Estrutura Organizacional da UFCA")
