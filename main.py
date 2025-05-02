448df187d6ec6cb6caec461707c890e6ca22b131
Felipe Cavalcante <felipexcavalcante@gmail.com>
2025-05-02 14:12:54 +0000

    add logo

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import streamlit as st
 import requests
 
-unidade = "122391" #UFCA
+unidade = "122391"  #UFCA
 
-estrutura = requests.get(f"http://estruturaorganizacional.dados.gov.br/doc/unidade-organizacional/{unidade}/estrutura")
+estrutura = requests.get(
+    f"http://estruturaorganizacional.dados.gov.br/doc/unidade-organizacional/{unidade}/estrutura"
+)
 estrutura = estrutura.json()
+
+st.title("Estrutura Organizacional da UFCA")
+st.image(
+    "https://upload.wikimedia.org/wikipedia/commons/8/8e/Logomarca_UFCA.svg",
+    width=100)
+st.write(
+    "Dados obtidos do [Portal da Transparência](http://www.portaltransparencia.gov.br"
+)
 st.write(estrutura)
